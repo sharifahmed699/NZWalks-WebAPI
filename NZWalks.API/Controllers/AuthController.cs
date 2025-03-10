98a6f97f9010968a3eb4ce3fadf17cea0c65d4c2
sharif <sharifahmedewu15@gmail.com>
2025-03-11 01:12:40 +0600

    inject jwt token repository

--- NZWalks.API/Controllers/AuthController.cs
+++ NZWalks.API/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Models.DTOs;
+using NZWalks.API.Repositories;
 
 namespace NZWalks.API.Controllers;
 
@@ -10,10 +11,12 @@ namespace NZWalks.API.Controllers;
 public class AuthController : ControllerBase
 {
     private readonly UserManager<IdentityUser> userManager;
+    private readonly ITokenRepository tokenRepository;
 
-    public AuthController(UserManager<IdentityUser> userManager)
+    public AuthController(UserManager<IdentityUser> userManager, ITokenRepository tokenRepository)
     {
         this.userManager = userManager;
+        this.tokenRepository = tokenRepository;
     }
     [HttpPost]
     [Route("Register")]
@@ -54,7 +57,18 @@ public class AuthController : ControllerBase
             var checkPassowrdResult = await userManager.CheckPasswordAsync(user, dto.Password);
             if (checkPassowrdResult)
             {
-                return Ok();
+                var roles=await userManager.GetRolesAsync(user);
+                if(roles != null)
+                {
+                   var token= tokenRepository.CreateJWTToken(user,roles.ToList());
+
+                    var response = new LoginResponseDto
+                    {
+                        JwtToken = token,
+                    };
+                    return Ok(response);
+                }
+               
             }
         }
 
--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fa1c2b01518fd6e7536f4961f3849b7e69a26a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89431655c950345d12821ebaab94a9c928b41b98")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
